11856e6858bacd1d7190678a25605acb24169a51
Thura Kyaw <104015611+Thura79@users.noreply.github.com>
2023-06-25 15:38:18 +0630

    Update functions.php

--- functions.php
+++ functions.php
@@ -69,3 +69,15 @@ function scanFile($dir):array{
 
     return $result;
 }
+
+
+function url(string $path = null) : string {
+    $url = $_SERVER["HTTPS"] ? "https" : "http";
+    $url .= "://";
+    $url .= $_SERVER["HTTP_HOST"];
+    if(isset($path)){
+        $url .= "/";
+        $url .= $path;
+    }
+    return $url;
+}
